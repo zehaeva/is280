13025fd134c0cc1acdd609f4478a2e93c1b3027c
Howard Canaway <howard.canaway@gmail.com>
2017-02-07 13:29:56 -0500

    personal updates

--- labs/07/CreateInvoice.php
+++ labs/07/CreateInvoice.php
@@ -9,7 +9,7 @@
         var secondItem = document.form[0].quantity2.value * document.form[0].rate2.value;
         var thirdItem = document.form[0].quantity3.value * document.form[0].rate3.value;
         
-        if (isNaN(firstItem) || isNaN(secondItem) || isNaN(thirdItem)) {
+        if (isNaN(firstItem)) {//} || isNaN(secondItem) || isNaN(thirdItem)) {
           alert("You can only enter numbers in the Quantity and Rate fields!");
           return false;
         }
@@ -27,6 +27,31 @@
 	</head>
 	<body>
 		<h1>Create Invoice</h1>
-		
+		<form action="" method=""> 
+      <p>
+        <label for="quantity1">Quantity</label>
+        <input type="text" name="quantity1" onchange="calcTotal"/>
+        <label for="rate1">Rate</label>
+        <input type="text" name="rate1" onchange="calcTotal" />
+        <label for="amount1">Amount</label>
+        <input type="text" name="amount1" />
+      </p>
+      <p>
+        <label for="quantity2">Quantity</label>
+        <input type="text" name="quantity2" />
+        <label for="rate2">Rate</label>
+        <input type="text" name="rate2" />
+        <label for="amount2">Amount</label>
+        <input type="text" name="amount2" />
+      </p>
+      <p>
+        <label for="quantity2">Quantity</label>
+        <input type="text" name="quantity3" />
+        <label for="rate1">Rate</label>
+        <input type="text" name="rate3" />
+        <label for="amount2">Amount</label>
+        <input type="text" name="amount3" />
+      </p>
+    </form>
 	</body>
 </html>
