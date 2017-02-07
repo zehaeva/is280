5d909f756ab510882c7adef1824716a55d50702b
Howard Canaway <howard.canaway@gmail.com>
2017-02-07 12:57:06 -0500

    Starting Scripting

--- labs/07/CreateInvoice.php
+++ labs/07/CreateInvoice.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict-dtd">
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>Invoices</title>
+		<title>Create Invoice</title>
 		<link rel="stylesheet" href="php_styles.css" type="text/css" />
+		<script type="text/javascript">
+      function calcTotal() {
+        var firstItem = document.form[0].quantity1.value * document.form[0].rate1.value;
+        var secondItem = document.form[0].quantity2.value * document.form[0].rate2.value;
+        var thirdItem = document.form[0].quantity3.value * document.form[0].rate3.value;
+        
+      }
+		</script>
 	</head>
 	<body>
-		<h1>Invoices</h1>
+		<h1>Create Invoice</h1>
 		
 	</body>
 </html>
