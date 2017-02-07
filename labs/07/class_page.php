7b892f2829fcfdb5271f76c9c673395557ec7976
Howard Canaway <howard.canaway@gmail.com>
2017-02-07 12:40:55 -0500

    Inital commit
    Just wanted to get something here

---
+++ labs/07/class_page.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict-dtd">
+ <html>
+	 <head>
+		<title>Invoices</title>
+		<link rel="stylesheet" href="php_styles.css" type="text/css" />
+	 </head>
+	 <body>
+		<h1>Invoices</h1>
+		<form method="get" action="CreateInvoice.php" enctype="application/x-www-form-urlencoded">
+			<p>
+				<input type="submit" value="Create Invoice" />
+			</p>
+		</form>
+		<form method="get" action="ViewOpenInvoices.php" enctype="application/x-www-form-urlencoded"> 
+			<p>
+				<input type="submit" value="View Open Invoices" />
+			</p>
+		</form>
+		<form method="get" action="ViewInvoices.php" enctype="application/x-www-form-urlencoded"> 
+			<p>
+				<input type="submit" value="View Invoices" />
+			</p>
+		</form>
+	 </body>
+ </html>
