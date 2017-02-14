a37ce77beaab978fc661cfe43c96a1ee04dd024f
Howard Canaway <howard.canaway@gmail.com>
2017-02-14 12:43:14 -0500

    Inital Add

---
+++ labs/09/BackupInvoices.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Backup Invoices</title>
+		<link rel="stylesheet" href="php_styles.css" type="text/css" />
+		<script type="text/javascript">
+		</script>
+	</head>
+	<body>
+		<h1>Backup Invoices</h1>
+	</body>
+</html>
