cda005595892c7882f544081f741170a962ca71d
Howard Canaway <howard.canaway@gmail.com>
2017-02-14 12:29:45 -0500

    New File!
    To view open invoices

---
+++ labs/09/ViewOpenInvoices.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>View Open Invoices</title>
+		<link rel="stylesheet" href="php_styles.css" type="text/css" />
+		<script type="text/javascript">
+		</script>
+	</head>s
+	<body>
+		<h1>View Open Invoices</h1>
+	</body>
+</html>
