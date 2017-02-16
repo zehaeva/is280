a6c86602d506026354b6531fa710a315d03fedb8
Howard Canaway <howard.canaway@gmail.com>
2017-02-16 12:49:21 -0500

    View Invoice
    Displays invoice!

--- labs/10/ViewInvoice.php
+++ labs/10/ViewInvoice.php
@@ -12,14 +12,14 @@
 				echo '<hr /><p>You must enter an existing Invoice Number. 
 				Click your brower back button to return to the Invoices page.</p><hr />';
 			}
-			else if (is_readable("Open\\". $_REQUEST['invoicenum'] .".txt")) {
+			else if (is_readable("open\\". $_REQUEST['invoicenum'] .".txt")) {
 			//	open the file
-				$invoice_fields = $fopen("Open\\". $_REQUEST['invoicenum'] .".txt", 'r');
+				$invoice_fields = fopen("open\\". $_REQUEST['invoicenum'] .".txt", 'r');
 				$bill_to = fgets($invoice_fields);
 				
 			//	now we're going to get the raw data of the first line into a more useful format
 				$fix_bill_to = explode('~', $bill_to);
-				$bill_to = implode('\n', $fix_bill_to);
+				$bill_to = implode("\n", $fix_bill_to);
 				$bill_to = stripslashes($bill_to);
 				
 			//	now we're going down each line and putting it into a variable
@@ -30,8 +30,8 @@
 				$items = array(array());
 				
 			//	gets the data for each line item
-				for($i=0; $i<=3; $i++) {
-					for($j=0; $j<=3; $j++) {
+				for($i=0; $i<4; $i++) {
+					for($j=0; $j<3; $j++) {
 						$items[$j][$i] = stripslashes(fgets($invoice_fields));
 					}
 				}
@@ -39,6 +39,44 @@
 				$total = stripslashes(fgets($invoice_fields));
 				
 				fclose($invoice_fields);
+				
+				echo '<h1>View Invoice</h1>';
+				echo "<hr /><br /><table frame='border' rules='rows'>";
+				echo "<tr><td><strong>Bill To</strong>";
+				echo "<pre>$bill_to</pre></td>";
+				echo "<td style='text-align:right' colspan=3>";
+				echo "<br /><strong>Invoice Number</strong>: $invoice_num<br />";
+				echo "<strong>Date</strong>: $date<br />";
+				echo "<strong>Terms</strong>: $terms<br /></td></tr>";
+				echo "<tr>";
+				$titles = array("Description", "Quantity", "Rate", "Amount");
+				echo "<td><strong>Description</strong><br />";
+				for ($i=0; $i<3; $i++) {
+					echo $items[$i][0] ."<br />";
+				}
+				echo "</td>";
+				echo "<td><strong>Quantity</strong><br />";
+				for ($i=0; $i<3; $i++) {
+					echo $items[$i][1] ."<br />";
+				}
+				echo "</td>";
+				echo "<td><strong>Rate</strong><br />";
+				for ($i=0; $i<3; $i++) {
+					echo $items[$i][2] ."<br />";
+				}
+				echo "</td>";
+				echo "<td><strong>Amount</strong><br />";
+				for ($i=0; $i<3; $i++) {
+					echo $items[$i][3] ."<br />";
+				}
+				echo "</td></tr>";
+				
+				echo "<tr><td colspan=4 style='text-align: right'><strong>Total</strong>: $total</td></tr>" ;
+				
+				echo "</table>";
+			}
+			else {
+				else "Could not read file!";
 			}
 		?>
 	</body>
