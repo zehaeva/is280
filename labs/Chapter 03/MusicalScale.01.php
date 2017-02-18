7c8cce50d6574b562f4ac3091ac89691dc1bb532
Howard Canaway <zehaeva@users.noreply.github.com>
2017-02-18 15:35:00 -0500

    Update MusicalScale.01.php
    Forgot to change the title

--- labs/Chapter 03/MusicalScale.01.php
+++ labs/Chapter 03/MusicalScale.01.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Die Roll</title>
+    <title>Musical Scale</title>
     <meta http-equiv="content-type" content="text/html; charset=iso-885901" />
   </head>
   <body>
