b21ef8d8ad911c0b10f2fd85e5570ea1bc623ddd
zehaeva <zehaeva@gmail.com>
2017-02-23 10:42:18 -0500

    DB Connection
    adding in the db connection file.

---
+++ project/inc/db.php
@@ -0,0 +1,4 @@
+<?php 
+     $conn_string = "host=localhost port=5432 dbname=MVGOClub user=goclub_site password=goclubreadonly";
+     $dbconn = pg_connect($conn_string);                                               
+?> 
