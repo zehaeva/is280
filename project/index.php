e25146089446656af8cd895205c5b4047ad23e26
zehaeva <zehaeva@gmail.com>
2017-02-23 10:39:31 -0500

    Update Index
    adding a db import to the file.

--- project/index.php
+++ project/index.php
@@ -1,4 +1,7 @@
 <?php
-  print("<div id="title">Welcome to the MV Go Club web page!</div>");
-  print("<div id="menu"><ul><li><a herf="members">Members</a></li></ul></div>);
+//include datahbase connection
+  include_once('inc/db.php');
+
+  print('<div id="title">Welcome to the MV Go Club web page!</div>');
+  print('<div id="menu"><ul><li><a herf="members">Members</a></li></ul></div>');
 ?>
