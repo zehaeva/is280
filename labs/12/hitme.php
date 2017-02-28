2f3c9fded0869f28782ffcfc2a6e1a0281ab66f0
Howard Canaway <hcanaway22@MVCCLABS.EDU>
2017-02-28 12:59:44 -0500

    Lab 12: Blackjack
    so much typing

--- labs/12/hitme.php
+++ labs/12/hitme.php
@@ -6,7 +6,7 @@
 		$hit = array_shift($deck);
 		$cards = implode(",", $deck);
 		
-		if (isset($current_hand[1)) {
+		if (isset($current_hand[1])) {
 			$player_hand = explode("~", $current_hand[1]);
 			$player_hand = $hit;
 			$card_count = 0;
@@ -40,9 +40,28 @@
 				else if (strpos($player_hand[$i], "Nine") !== False) {
 					$card_count += 9;
 				}
+				// I refuse to specify King, Queen, or Jack when they're 
+				// all going to be the same value
 				else {
 					$card_count += 10;
 				}
+			}
+			
+			$cards = $cards . implode("~", $player_hand);
+			
+			if ($aces > 0) {
+				for ($i=1; $i <= $aces; $i++) {
+					if ($card_count + 11 <= 21) {
+						$card_count += 11;
+					}
+					else {
+						$card_count += 1;
+					}
+				}
+			}
+			
+			if ($card_count == 21) {
+				$cards = $cards ." You win!";
 			}
 		}
 	}
