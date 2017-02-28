73aa8adcfddaafac5f0bc5a688638d2c68beadfa
Howard Canaway <hcanaway22@MVCCLABS.EDU>
2017-02-28 12:49:08 -0500

    Lab 12: Blackjack
    building up hitme.php

--- labs/12/hitme.php
+++ labs/12/hitme.php
@@ -1,3 +1,34 @@
 <?php
-	echo "hit me";
+//	check to see that file exists
+	if (file_exists("hand.txt") && filesize("hand.txt") > 0) {
+		$current_hand = file("hand.txt");
+		$deck = explode(",", $current_hand[0]);
+		$hit = array_shift($deck);
+		$cards = implode(",", $deck);
+		
+		if (isset($current_hand[1)) {
+			$player_hand = explode("~", $current_hand[1]);
+			$player_hand = $hit;
+			$card_count = 0;
+			$aces = 0;
+			
+			for ($i=0; $i < count($player_hand); $i++) {
+				if (strpos($player_hand[$i], "Ace") !== False) {
+					$aces++;
+				}
+				else if (strpos($player_hand[$i], "King") !== False) {
+					
+				}
+				else if (strpos($player_hand[$i], "Queen") !== False) {
+					
+				}
+				else if (strpos($player_hand[$i], "Jack") !== False) {
+					
+				}
+				else {
+					
+				}
+			}
+		}
+	}
 ?>
