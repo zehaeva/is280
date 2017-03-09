6a3f243d310007c049e73c9b837124ae65756fcf
Howard Canaway <zehaeva@users.noreply.github.com>
2017-03-09 08:20:41 -0500

    Create NumberForm.01.php

---
+++ labs/Chapter04/NumberForm.01.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Number Form</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-885901" />
+  </head>
+  <body>
+    <?php
+      $displayform = TRUE;
+      $number = "";
+      
+      if (isset($_POST['Submit'])) {
+        $number = $_POST['Number'];
+
+        if (is_numeric($number)) {
+          $displayform = FALSE;
+        } 
+        else {
+          echo "<p>You need to enter a numeric value.</p>\n";
+          $displayform = TRUE;
+        }
+      }
+      
+      if ($displayform) {
+    ?>
+      <form name="NumberForm" action="NumberForm.php" method="post">
+        <p>Enter a number: <input type="text" name="Number" value="<?php echo $number; ?>" /></p>
+        <p><input type="reset" value="Clear Form" />&nbsp;&nbsp;<input type="submit" name="Submit" value="Send Form" /></p>
+      </form>
+    <?php
+      }
+      else {
+        echo "<p>Thank you for entering a number.</p>\n";
+        echo "<p>Your number, $number, squared is ". ($number*$number) .".</p>\n ";
+        echo "<p><a href=\"NumberForm.php\">Try again?</a></p>\n";
+      }
+    ?>
+  </body>
+</html>
