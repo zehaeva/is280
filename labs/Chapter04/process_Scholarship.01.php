2099f13305570d1194d21dd4836eb27cefe60a6a
Howard Canaway <zehaeva@users.noreply.github.com>
2017-03-09 07:54:20 -0500

    Create process_Scholarship.01.php

---
+++ labs/Chapter04/process_Scholarship.01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Scholarship Form</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-885901" />
+  </head>
+  <body>
+    <?php
+        $first_name = $_POST['fName'];
+        $last_name = $_POST['lName'];
+
+        echo "Thank you for filling out the scholarship form,". $first_name. " ". $last_name .".";
+    ?>
+  </body>
+</html>
