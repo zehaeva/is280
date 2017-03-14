621060e8fb6c150708dc4b3dbb7f7ed7098fa6d0
zehaeva <zehaeva@gmail.com>
2017-03-14 08:33:42 -0400

    Project
    forgot the index page for the api

---
+++ project/api/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MV Go Club Games</title>
+		<script src="https://unpkg.com/vue/dist/vue.js"></script>
+		<?php include_once('../bootstrap-cdn.php'); ?>
+	</head>
+	<body>
+<?php
+  include('../menu.php');
+  print(menu(5));
+?>
+		<div class="container">
+			<div id="title">MV Go Club API</div>
+			<div><p>Here at the MV Go Club we believe in the openess of data, so we have implimented an API with which you can interact with the data that is on this site directly.</p></div>
+			<div>
+				<ul>
+				<li>
+					<dl>
+						<dt><a href="/api/games/"><?php echo $_SERVER['SERVER_NAME']; ?>/api/games/</a></dt>
+						<dd>This endpoint returns all the games in the database, you can ask for a specific game by adding the id of the game at the end of the endpoint. e.g. <a href="/api/games/1"><?php echo $_SERVER['SERVER_NAME']; ?>/api/games/1</a></dd>
+					</dl>
+				</li>
+				<li><a href="/api/members/"><?php echo $_SERVER['SERVER_NAME']; ?>/api/members/</a></li>
+				</ul>
+			</div>
+			
+			<script src="http://<?php echo $_SERVER['SERVER_NAME'] .'/js/mvgoclub.js'; ?>" type="text/javascript"></script>
+		</div>
+	</body>
+</html>
