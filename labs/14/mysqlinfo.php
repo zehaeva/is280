8d8817706835c891456f1f0de7e5e003ad3e87bd
Howard Canaway <hcanaway22@MVCCLABS.EDU>
2017-03-21 12:51:48 -0400

    Week 9: mysqlinfo
    page is complete, I guess

--- labs/14/mysqlinfo.php
+++ labs/14/mysqlinfo.php
@@ -1,3 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MySQL Server Information</title>
+		<link rel="stylesheet" src="php_styles.css" type="text/css" />
+	<head>
+	<body>
+		<h1>MySQL DB Server Information</h1>	
 <?php
-
+$connect = mysqli_connect('localhost', 'root', '');
+echo "<p>MySQL Client Version: ". mysqli_get_client_info() .'</p>';
+echo "<p>MySQL Connection: ". mysqli_get_host_info($connect) .'</p>';
+echo "<p>MySQL Protocol Version: ". mysqli_get_proto_info($connect) .'</p>';
+echo "<p>MySQL Server Version: ". mysqli_get_server_info($connect) .'</p>';
+mysqli_close($connect);
 ?>
+	</body>
+</html>
