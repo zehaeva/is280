8f130f565b656db5c1129b0fbbc32e2a0a303966
Howard Canaway <hcanaway22@mvcclabs.edu>
2017-03-30 12:28:37 -0400

    Create RegisterDiver page
    page updates the database with the submitted registation info

---
+++ labs/14/registerdiver.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Register Diver</title>
+		<link rel="stylesheet" src="php_styles.css" type="text/css" />
+	</head>
+	<body>
+		<h1>Seaside Shelly's Scuba School</h1>
+		<h2>Registration Confirmation</h2>
+		<?php
+			$diver_id = @$_REQUEST['diver_id'];
+			
+			if (empty($diver_id)) {
+				exit('<p>You must enter a valid DiverID in the proper field, please return to the page and enter your DiverID</p>');
+			}
+			
+			include('functions.php');
+			$conn = @mysqli_connect("localhost", "root", "") or die(dbconnerror($conn));
+			
+			$dbname = 'scubaschool';
+			
+			if(!@mysqli_select_db($conn, $dbname)) {
+				initialize_db($conn, $dbname);
+				@mysqli_select_db($conn, $dbname);
+			}
+			
+			$table_name = 'registration';
+			
+			$class = @$_REQUEST['class'];
+			$days = @$_REQUEST['days'];
+			$time = @$_REQUEST['time'];
+			
+			$sql = 'INSERT INTO '. $table_name .' (diverid, class, days, time) VALUES (?, ?, ?, ?)';
+			
+			$params = array($diver_id, $class, $days, $time);
+				
+			$results = mysqli_prepared_query($conn, $sql, "ssss", $params);
+			
+			mysqli_close($conn);
+		?>
+		<p>You are registered for <?php echo $class .' on '. $days .', '. $time; ?>. Click your browser's back button to register for another course or review your schedule.</p>
+	</body>
+</html>
